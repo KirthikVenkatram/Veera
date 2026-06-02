d59afdfef240d2136971cf9257ec05ff2005d055
KirthikVenkatram <kirthikvenkatram255@gmail.com>
2026-06-03 00:39:27 +0530

    Initial Commit

---
+++ veeraTests/veeraTests.swift
@@ -0,0 +1,19 @@
+//
+//  veeraTests.swift
+//  veeraTests
+//
+//  Created by Kirthik Venkatram on 03/06/26.
+//
+
+import Testing
+@testable import veera
+
+struct veeraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
